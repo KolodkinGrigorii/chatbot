fe47b55ddad4fbed5d261ed0fab8945a6795158e
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2025-10-12 14:53:12 +0300

    fix2

--- bot/__main__.py
+++ bot/__main__.py
@@ -9,12 +9,18 @@ def main() -> None:
             updates = bot.telegram_client.getUpdates(next_update_offset)
             bot.database_client.persist_updates(updates)
             for update in updates:
-                if "message" in update and "text" in update["message"]:
-                    bot.telegram_client.sendMessage(
-                        chat_id=update["message"]["chat"]["id"],
-                        text=update["message"]["text"],
-                    )
-                    print(".", end="", flush=True)
+                message = update.get("message")
+                if message is not None:
+                    chat_id = message.get("chat", {}).get("id")
+                    text = message.get("text")
+                    if chat_id is not None and text is not None:
+                        bot.telegram_client.sendMessage(
+                            chat_id=chat_id,
+                            text=text,
+                        )
+                        print(".", end="", flush=True)
+                    else:
+                        print("error", end="", flush=True)
                 else:
                     print("error", end="", flush=True)
                 next_update_offset=max(next_update_offset, update["update_id"]+1)
